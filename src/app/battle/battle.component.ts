8d6b463235574790b0dd06a34f362ed8385c1910
Robert <robertjohnconcepcion@gmail.com>
2019-06-25 00:19:52 +0800

    battle function complete

--- src/app/battle/battle.component.ts
+++ src/app/battle/battle.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, Input } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { Icon } from  '../Icon';
 import { IconService } from '../icon.service';
 import { BattleService } from '../battle.service';
 import { ActivatedRoute } from '@angular/router';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-battle',
@@ -11,19 +12,32 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./battle.component.sass']
 })
 export class BattleComponent implements OnInit {
+
+
+  showloader :boolean = true;
+  texty : string;
+
   creator : string;
   creatorIcon : string;
   creatorHp : number;
+  creatorHpPercent : number = 100;
   icon : Icon[];
   enemy : Icon[];
 
   enemyHp : number;
+  enemyHpPercent : number = 100;
   enemyCurrentAtk : string;
   enemyAttackBg = ['rgb(255, 76, 76)','rgb(102, 102, 255)','rgb(69, 162, 69)'];
+  enemyAttackPosition = [
+    ['10px', '33px'],
+    ['100px', '33px'],
+    ['55px', '97px'],      
+  ];
   enemyBgCounter : number = 0;
 
   attackList = ['rock','paper','scissors'];
 
+  getHit : number = 0;
 
   constructor(
     private cookieService: CookieService,
@@ -33,16 +47,29 @@ export class BattleComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.creator = this.cookieService.get('creator');
-    
-    this.route.paramMap.subscribe(params => {
-      this.creatorIcon = params.get('fakeIconId');
-    });
 
+    if(this.cookieService.check('creator')){
+      this.creator = this.cookieService.get('creator');
+      this.route.paramMap.subscribe(params => {
+        this.creatorIcon = params.get('fakeIconId');
+      });
+      this.setMyIcom();
+    }else{
+        this.texty = "Dont be shy set your name";
+    }
+
+  }
+
+
+  setMyIcom() : void {
+    this.showloader = true;
+    this.texty = "Creating bonding between you and the selected icon.";
     this.iconService.searchIconById(this.creatorIcon).subscribe(
       (response : any) => {
+        this.showloader = false;
         this.icon = response[0];
         this.creatorHp = response[0].hp;
+        this.searchEnemy();
       },
       (error: any) => {
         console.log('error');
@@ -52,28 +79,38 @@ export class BattleComponent implements OnInit {
         console.log('complete');
       }
     );
-    this.searchEnemy();
   }
 
   searchEnemy() : void {
+    this.showloader = true;
+    this.texty = "Searching your opponent icon";    
     this.battleService.enemiesList()
     .subscribe(
       (response : any) => {
         let enemy = response[Math.floor(Math.random() * response.length)];
         this.enemy = enemy;
         this.enemyHp = enemy.hp;
+        this.showloader = false;
       }
     );
   }
 
   enemyAttack()  {
     this.enemyCurrentAtk = this.attackList[this.iconService.rand(0,2)];
-    return this.enemyCurrentAtk;
+    
+    setTimeout(function(){
+      this.enemyCurrentAtk = null;
+    },2000);
+     return this.enemyCurrentAtk;
+  }
+
+  _dmgPercentCalc (value: number, total : number) : number {
+   return (value/total) * 100;
   }
 
   creatorAtk(atkType : number)  {
 
-    let battle = this.battleResult(this.attackList[atkType],this.enemyAttack());
+    var battle = this.battleResult(this.attackList[atkType],this.enemyAttack());
     
     
     this.enemyBgCounter += 1;
@@ -81,10 +118,33 @@ export class BattleComponent implements OnInit {
       this.enemyBgCounter = 0;
     }
     
-    console.log(battle);
-    
+    this.getHit = battle.win;
 
+    if(battle.win == 1){
+      let dmg = this._dmgPercentCalc(battle.dmg,this.enemyHp);
+
+      if(dmg > this.enemyHpPercent){
+        this.enemyHpPercent = 0;
+      }else{
+        this.enemyHpPercent -= dmg;
+      }
+
+    }else if(battle.win == 2){
+
+      let dmg = this._dmgPercentCalc(battle.dmg,this.creatorHp);
+
+      if(dmg > this.creatorHpPercent){
+        this.creatorHpPercent = 0;
+      }else{
+        this.creatorHpPercent -= dmg;
+      }
+
+    }else{
+
+    }
   }
+
+
 
   battleResult(p1Atk : string, p2Atk : string) {
     let result = {};
