4bfe080062a4f26055e829846c0f3d7d9b9d6e1f
Robert John Concepcion <Robert.Concepcion@nowcom.com.ph>
2019-07-01 12:09:09 +0800

    fix confirm password in registration

--- src/app/player.service.ts
+++ src/app/player.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Player } from './player';
 
+
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
@@ -18,9 +19,24 @@ export class PlayerService {
 
   constructor(private http: HttpClient) { }
 
-
   addPlayer (player: Player): Observable<Player> {
     const url = `${environment.endpoint}player`;
     return this.http.post<Player>(url, player, httpOptions);
   }
+
+  findPlayer (username: any): Observable<Player[]>{    
+    const url = `${environment.endpoint}player?q={"username":"${username.trim()}"}`;
+    return this.http.get<Player[]>(url,httpOptions);
+  }
+
+  // searchIcon (term: string): Observable<Player[]>{
+    
+  //   if (!term.trim()) {
+  //     const url = `${environment.endpoint}player`;
+  //     return this.http.get<Player[]>(url,httpOptions)
+  //   }
+    
+  //   return this.http.get<Player[]>(url,httpOptions)
+  // }
+
 }
--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Player } from  '../player';
+import { Icon } from '../icon';
+import { IconService } from '../icon.service';
 import { PlayerService } from '../player.service';
 import { ActivatedRoute } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
-import { FormControl, FormGroup, FormBuilder, Validators, FormArray, NgControl } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators, FormArray, NgControl, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-player',
@@ -11,6 +13,12 @@ import { FormControl, FormGroup, FormBuilder, Validators, FormArray, NgControl }
   styleUrls: ['./player.component.sass']
 })
 export class PlayerComponent implements OnInit {
+
+  showloader = false;
+  texty : string = '';
+  errors : boolean = false;
+  errors_msg = [];
+  usernameExist : boolean;
 
   playerForm = this.pf.group({
     username : ['', {
@@ -24,74 +32,76 @@ export class PlayerComponent implements OnInit {
     password : ['', {
       validators : [
         Validators.required,
-        Validators.minLength(6),
+        Validators.minLength(6),   
       ]
     } ],
     password2 : ['', {
       validators : [
         Validators.required,
         Validators.minLength(6),
-        this.confirmPassword,
+        this.MatchPassword,
       ]
     } ]
   });
   
   constructor(
     private playerService: PlayerService,
+    private iconService: IconService,    
     private route: ActivatedRoute,
     private pf: FormBuilder,
   ) { }
-
-  showloader = false;
-  creator : Player[];
-
-  player : any = {};
 
   ngOnInit() {
 
   }
 
-  // ngOnDestroy() {
-  //   //console.log(this.creator);
-  // }
+  onSubmit() : void {
 
-  // onSubmit() : void {
-  //   console.log(this.playerForm.value);
-  // }
+    let username = this.playerForm.get('username').value;
+    this.errors =  false;
+    this.showloader = true;
+    this.texty = "Validating the uniqueness of the name..";
+    this.playerService.findPlayer(username)
+    .subscribe((response : any)=>{
 
-  // _rePasswordChecking() : boolean {
-  //   return false;
-  // }
+      if(!response.length){
+        this.texty = "Creating your account...";
+        this.playerService.addPlayer(this.playerForm.value as Player)
+        .subscribe((response: any) => {
+          this.showloader = false;
+        },(error)=>{
+          console.log(error);
+        })
 
-  banWords(control: FormControl){
-
-    let username = control.value;
-    if(username == 'sa'){
-      return {
-        banWords : 'You cant use that word'
+      }else{
+        
+        this.errors_msg = ["The name " + username + " is already exist, please try a unique name"];
+        this.errors = true;
+        this.showloader = false;
       }
-    }
-    return null;
+    },(error)=>{
+
+    });
   }
 
-  confirmPassword(control : FormGroup) {
-    let password = control.root.get('password');
-    let password2 = control.value;
 
-
-
-    if(password != null){
-
-      if(password.value != password2){
-
-        return {
-          notMatch : {msg:'Password not match'}
-        }
-      }
-
+  MatchPassword(control: AbstractControl) {
+    if (!control.value) {
       return null;
     }
+    let password = control.root.get('password').value;
+    let confirmPassword = control.root.get('password2').value;
+     if(password != confirmPassword) {
+         return {
+          notMatch : {msg:'Password not match'}
+        }
+     } else {
+         return null
+     }
+ }
 
+  test(test : any) {
+    console.log(test);
   }
-
+  
 }
